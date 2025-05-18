6d263efdbea2773b4be4c31e875e648adda2e0ae
yorjaggy <yorjaggy@gmail.com>
2025-05-18 14:24:49 -0500

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+# In this file put all the logic to crete the proper infraestructure
+terraform {
+    required_providers {
+        # Add the provideres according to the challenges
+    }
+}
+
+
+# Add the resources relatedo to the provider
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+# In this file put the variables related to the deployment
+variable "variable_name" {
+    type = "type",
+    description = "Description"
+}
